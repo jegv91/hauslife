bf96ebe6fec21b2b1fe2b7b14ecb91e62c437038
JEGV <jegv91@gmail.com>
2012-11-18 10:15:08 -0600

    Mejoras en eventos

--- views/listaEventos.php
+++ views/listaEventos.php
@@ -12,7 +12,15 @@ $listaProximos=$evento->getInfoEventosProximos(date("Y-m-d"),$_SESSION['user_idE
 <div class="container-fluid">
 	<div class="row-fluid">
 		<div class="span6 offset3">
+			<div style="display:table-cell;">
 			<h1>Eventos Pr&oacute;ximos</h1>
+			</div>
+			<div style="display:table-cell; padding-left:180px;">			
+			<?php if($tipo==0){?>
+			<a class='btn' href='registraEvento.php'><i class='icon-glass'></i>Registrar
+				evento</a>
+			<?php }?>
+			</div>
 			<table class="table table-bordered">
 				<tr>
 					<th colspan=3>Titulo</th>
@@ -23,10 +31,10 @@ $listaProximos=$evento->getInfoEventosProximos(date("Y-m-d"),$_SESSION['user_idE
 					echo "<tr><td>".$obj["titulo"]."</td>";
 					echo "<td><a class='btn btn-primary' href='verInformacionEvento.php?id=".$obj["_id"]."'><i class='icon-glass icon-white'></i> Ver Informaci&oacute;n</a></td>";
 					if($tipo==0){
-						echo "<td><a class='btn btn-danger' href='../controllers/eliminarEvento.php?id=".$obj["_id"]."'><i class='icon-trash icon-white'></i> Confirmar evento</a></td></tr>";
+						echo "<td><a class='btn btn-danger' href='../controllers/eliminarEvento.php?id=".$obj["_id"]."'><i class='icon-trash icon-white'></i> Eliminar evento</a></td></tr>";
 					} elseif ($tipo == 1) {
 						echo "<td><a class='btn btn-success' href='../controllers/confirmarEvento.php?id=".$obj["_id"]."'><i class='icon-thumbs-up icon-white'></i> Confirmar Evento</a></td>";
-						echo "<td><a class='btn btn-warning' href='../controllers/rechazarEvento.php?id=".$obj["_id"]."'><i class='icon-thumbs-down icon-white'></i> Rechazar Evento</a></td>";	
+						echo "<td><a class='btn btn-warning' href='../controllers/rechazarEvento.php?id=".$obj["_id"]."'><i class='icon-thumbs-down icon-white'></i> Rechazar Evento</a></td></tr>";	
 					}
 				}}
 				?>
@@ -42,18 +50,15 @@ $listaProximos=$evento->getInfoEventosProximos(date("Y-m-d"),$_SESSION['user_idE
 					echo "<tr><td>".$obj["titulo"]."</td>";
 					echo "<td><a class='btn btn-primary' href='verInformacionEvento.php?id=".$obj["_id"]."'><i class='icon-glass icon-white'></i> Ver Informaci&oacute;n</a></td>";
 					if($tipo==0){
-						echo "<td><a class='btn btn-danger' href='../controllers/eliminarEvento.php?id=".$obj["_id"]."'><i class='icon-trash icon-white'></i> Eliminar evento</a></td></tr>";
+						echo "<td><a class='btn btn-success' href='../controllers/subirFotos.php?id=".$obj["_id"]."'><i class='icon-camera icon-white'></i> Subir fotos</a></td>";
+						echo "<td><a class='btn btn-success' href='../controllers/retroEventos.php?id=".$obj["_id"]."'><i class='icon-star icon-white'></i> Retroalimentar</a></td></tr>";
 					} elseif ($tipo == 1) {
-						echo "<td><a class='btn btn-success' href='../controllers/confirmarEvento.php?id=".$obj["_id"]."'><i class='icon-thumbs-up icon-white'></i> Confirmar Evento;n</a></td>";
-						echo "<td><a class='btn btn-warning' href='../controllers/rechazarEvento.php?id=".$obj["_id"]."'><i class='icon-thumbs-up-down icon-white'></i> Rechazar Evento;n</a></td>";	
+						echo "<td><a class='btn btn-success' href='../controllers/subirFotos.php?id=".$obj["_id"]."'><i class='icon-camera icon-white'></i> Ver fotos</a></td></tr>";
 					}
 				}}
 				?>
 			</table>
-			<?php if($tipo==0){?>
-			<a class='btn' href='registraEvento.php'><i class='icon-glass'></i>Registrar
-				evento</a>
-			<?php }?>
+			
 		</div>
 	</div>
 </div>
